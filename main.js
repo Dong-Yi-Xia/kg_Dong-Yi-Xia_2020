3b0545510266628989582d00534c9068d11bac45
Dong-Yi-Xia <dong7777xia@gmail.com>
2021-04-26 17:26:39 -0400

    completed integers to phonetic function

--- main.js
+++ main.js
@@ -12,8 +12,40 @@ let numbers = require('./input_arg')
 
 // pseudocode
 // create a map to match the number to its phonetic 
-// loop through the array of integer and convert them into a string
-// loop through string number and get its phonetic equivalent
+// loop through the array of integers and convert them into a string
+// loop through the string number and get its phonetic equivalent
 //   within the second loop, string concatenate them
 //   after the end of the second loop, push it into an new array
 // convert the new array into a string format  
+
+
+const integersToPhonetic = (arr) => {
+    let map = {
+        "0" : "Zero",
+        "1" : "One",
+        "2" : "Two",
+        "3" : "Three",
+        "4" : "Four",
+        "5" : "Five",
+        "6" : "Six",
+        "7" : "Seven",
+        "8" : "Eight",
+        "9" : "Nine",
+    }
+
+    let strWord = []
+
+    for(let num of arr){
+        let strNum = num.toString()
+        let word = ''
+        for(let char of strNum){
+            word += map[char]
+        }
+        strWord.push(word)
+    }
+
+    return strWord.join(',')
+} 
+
+
+console.log( integersToPhonetic(numbers) )
