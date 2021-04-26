6ab0d304f64c2a8cb4d708328603d926fd4f6ddb
Dong-Yi-Xia <dong7777xia@gmail.com>
2021-04-26 17:05:00 -0400

    wrote out pseudocode

--- main.js
+++ main.js
@@ -8,4 +8,12 @@ let numbers = require('./input_arg')
 // Given an array: [3, 25, 209], print “Three,TwoFive,TwoZeroNine” into stdout.
 // Given an array: [10, 300, 5], print “OneZero,ThreeZeroZero,Five” into stdout.
 
-console.log(numbers)
+// console.log(numbers)
+
+// pseudocode
+// create a map to match the number to its phonetic 
+// loop through the array of integer and convert them into a string
+// loop through string number and get its phonetic equivalent
+//   within the second loop, string concatenate them
+//   after the end of the second loop, push it into an new array
+// convert the new array into a string format  
